9d5d3ceabe009f0f19a4bcb0fa7a2e05a070d315
debashishmodak <dmoda87@gmail.com>
2017-03-20 14:50:52 +0530

    Modifying TestHelloWorld.java

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -4,17 +4,17 @@ import org.junit.Test;
 
 public class TestHelloWorld {
 	HelloWorld htest = new HelloWorld();
-	int c=10;
-	int d=20;
+	int c=20;
+	int d=30;
 	
 	@Test
 	public void testSum(){
-		assertEquals(30, htest.sum(c, d));
+		assertEquals(50, htest.sum(c, d));
 	}
 	
 	@Test
 	public void testMultiply(){
-		assertEquals(200, htest.multiply(c, d));
+		assertEquals(600, htest.multiply(c, d));
 	}
 
 }
